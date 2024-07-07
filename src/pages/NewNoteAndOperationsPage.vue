57de0caa75b5f43868a56eba6b3cf22753af5af7
Jean Gomes <jeangomes.ti@gmail.com>
2024-07-07 18:12:02 -0300

    fix: change icon

--- src/pages/NewNoteAndOperationsPage.vue
+++ src/pages/NewNoteAndOperationsPage.vue
@@ -30,7 +30,7 @@
           <template v-slot:body="props">
             <q-tr :props="props">
               <q-td auto-width>
-                <q-btn size="sm" color="accent" round dense @click="props.expand = !props.expand" :icon="props.expand ? 'remove' : 'add'" />
+                <q-btn size="sm" color="accent" round dense @click="props.expand = !props.expand" :icon="props.expand ? 'expand_less' : 'expand_more'" />
               </q-td>
               <q-td
                 v-for="col in props.cols"
